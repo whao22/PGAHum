f923d36f732fec6a04ffbae5bb0e6ba2021bee1a
whao22 <204757644@qq.com>
2024-03-02 20:45:21 +0800

    update v1

--- scripts/infer_377_hfavatar.sh
+++ scripts/infer_377_hfavatar.sh
@@ -1,4 +1,4 @@
-exp_name="CoreView_377_1707616975_slurm_mvs_w/_inner-w/_init_sdf-w/_patch-rgb50-lpip5-eik0.1"
+exp_name="CoreView_377_1707616975"
 conf="confs/hfavatar-zju/ZJUMOCAP-377-mono-4gpus.conf"
 base_exp_dir="exp/${exp_name}"
 
--- scripts/train_377_hfavatar.sh
+++ scripts/train_377_hfavatar.sh
@@ -1,8 +1,8 @@
-user_comment='mono_v007_tri-scale1_nn_intermask'
+user_comment='mono_1_1_3_true'
 exp_comment=`date +%s`
 run_name="CoreView_377_${exp_comment}_run_${user_comment}"
 base_exp_dir="exp/${run_name}"
-conf="confs/hfavatar-zju/ZJUMOCAP-377-mono-4gpus.conf"
+conf="confs/hfavatar-zju/ZJUMOCAP-377-4gpus.conf"
 log_name="CoreView_377_${exp_comment}_run.log"
 echo The base experiment directory is ${base_exp_dir}.
 
