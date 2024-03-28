a6fed5a09b0234244db51d48ccbee6c727f80e31
whao22 <204757644@qq.com>
2024-03-29 00:32:18 +0800

    update scripts

---
+++ scripts/people_snapshot/infer_female-4-casual_hfavatar.sh
@@ -0,0 +1,10 @@
+conf="confs/hfavatar-people_snapshot/PeopleSnapshot-female-4-casual-mono-4gpus.conf"
+base_exp_dir="exp/CoreView_male-4-casual_1709841604_run_mono_1_1_3_true"
+
+python infer.py \
+    --conf ${conf} \
+    --base_exp_dir ${base_exp_dir} \
+    --resolution_level 2 \
+    --infer_mode gup \
+    --novel_pose data/AIST++/motions/gBR_sBM_cAll_d04_mBR0_ch01.pkl \
+    --novel_pose_type aistplusplus_odp
---
+++ scripts/synthetic_human/infer_manuel_hfavatar.sh
@@ -0,0 +1,16 @@
+conf="confs/hfavatar-synthetic_human/SyntheticHuman-manuel-4gpus.conf"
+base_exp_dir="exp/SyntheticHuman-megan_1711177582_slurm_mono_1_1_1_true"
+
+# advanced motion
+# data/AIST++/motions/gBR_sFM_cAll_d05_mBR2_ch09.pkl
+
+# basic motion
+# data/AIST++/motions/gBR_sBM_cAll_d04_mBR1_ch06.pkl
+
+python infer.py \
+    --conf ${conf} \
+    --base_exp_dir ${base_exp_dir} \
+    --resolution_level 4 \
+    --infer_mode nvs \
+    --novel_pose data/AIST++/motions/gBR_sFM_cAll_d05_mBR2_ch09.pkl \
+    --novel_pose_type aistplusplus_odp
