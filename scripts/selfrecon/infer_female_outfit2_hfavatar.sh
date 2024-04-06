1bbb7a5eca9fe3aad91583dde335fd75a4ecf321
whao22 <204757644@qq.com>
2024-04-06 19:46:11 +0800

    update scripts

---
+++ scripts/selfrecon/infer_female_outfit2_hfavatar.sh
@@ -0,0 +1,16 @@
+conf="confs/hfavatar-selfrecon_synthesis/SelfreconSynthesis-male-outfit2-mono-4gpus.conf"
+base_exp_dir="exp/SelfreconSynthesis-male_outfit2_1711697167_slurm_mono_1_1_3_true"
+
+# advanced motion
+# data/AIST++/motions/gBR_sFM_cAll_d05_mBR2_ch09.pkl
+
+# basic motion
+# data/AIST++/motions/gBR_sBM_cAll_d04_mBR1_ch06.pkl
+
+python infer.py \
+    --conf ${conf} \
+    --base_exp_dir ${base_exp_dir} \
+    --resolution_level 2 \
+    --infer_mode unseen \
+    --novel_pose data/AIST++/motions/gBR_sFM_cAll_d05_mBR2_ch09.pkl \
+    --novel_pose_type aistplusplus_odp
